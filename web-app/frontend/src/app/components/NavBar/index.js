c9744ad58a992b3b80f8f89e936328d925990597
Shreyas Honnalli <sc20sh@leeds.ac.uk>
2023-10-21 21:14:28 +0100

    l

--- web-app/frontend/src/app/components/NavBar/index.js
+++ web-app/frontend/src/app/components/NavBar/index.js
@@ -3,20 +3,35 @@ import Image from "next/image";
 import Logo from "../../images/logo.png";
 
 export default function NavBar() {
-  return (
-    <div className="w-full flex justify-between-items-center border b-2 border-gray-200 py-2 px-4">
-      <Link href="/">
-        <div>
-          <Image
-            className="cursor-pointer"
-            src={Logo}
-            alt="Logo"
-            Layout="responsive"
-            height={50}
-            width={50}
-          />
-        </div>
-      </Link>
-    </div>
-  );
+	return (
+		<div className="w-full flex justify-between items-center border-2 border-gray-200 py-2 px-4">
+			<Link href="/">
+				<div className="mr-4">
+					<Image
+						className="cursor-pointer"
+						src={Logo}
+						alt="Logo"
+						layout="responsive"
+						height={50}
+						width={50}
+					/>
+				</div>
+			</Link>
+
+			<text
+				style={{
+					fontFamily: "Courier New",
+					fontSize: "50px",
+					letterSpacing: "5px",
+				}}
+			>
+				socialBTC
+			</text>
+
+			{/* NFT Image Placeholder */}
+			<div className="w-16 h-16 bg-gray-300 rounded-full">
+				{/* You can place the NFT image here */}
+			</div>
+		</div>
+	);
 }
