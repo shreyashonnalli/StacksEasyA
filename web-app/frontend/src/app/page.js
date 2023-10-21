f3d0e2971ca8202264a33a12ab62faff1c315e14
Devan Govind <devangovind@gmail.com>
2023-10-22 00:47:39 +0100

    feat: finished feed

--- web-app/frontend/src/app/page.js
+++ web-app/frontend/src/app/page.js
@@ -2,13 +2,15 @@ import ContentCard from "./components/ContentCard";
 import NavBar from "./components/NavBar";
 import SideBar from "./components/SideBar";
 import { BluePost } from "./images/feeds/all-posts";
+import { content } from "./images/feeds";
 
 export const metadata = {
   title: "BitLoan",
   description: "A decentralized Bitcoin lending application",
 };
 
-export default function Home({ content }) {
+export default function Home() {
+  const content2 = content;
   return (
     <>
       <NavBar />
@@ -16,17 +18,14 @@ export default function Home({ content }) {
         <div className="h-[92vh] overflow-hidden xl:hover:overflow-auto">
           <SideBar />
         </div>
-        <div className="mt-4 flex flex-col gap-10 overflow-auto h-[88vh] videos flex-1">
+        <div className="mt-4 flex flex-col gap-10 overflow-auto h-[88vh] videos flex-1 mr-20">
           <h1 className="mt-8 text-4xl text-center">Home Page</h1>
-          {content?.length ? (
-            content.map((item) => <ContentCard post={item} key={item.id} />)
+
+          {content2?.length ? (
+            content2.map((item) => <ContentCard post={item} key={item.id} />)
           ) : (
             <text>No results</text>
           )}
-          <h1 className="mt-8 text-4xl text-center">Lagoon</h1>
-          <p className="mt-4 text-center">
-            Decentralized lending and borrowing with sBTC.
-          </p>
         </div>
       </div>
     </>
