7c3fe74cdf2a6901b966e532451663c15ca1a0c0
Shreyas Honnalli <sc20sh@leeds.ac.uk>
2023-10-22 11:51:09 +0100

    s

--- web-app/frontend/next.config.js
+++ web-app/frontend/next.config.js
@@ -1,8 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: ["cdn.gobankingrates.com", "png.pngtree.com"],
-  },
+	images: {
+		domains: [
+			"cdn.gobankingrates.com",
+			"png.pngtree.com",
+			"image-cdn.hypb.st",
+			"thumbor.forbes.com",
+			"q5g9j9b9.stackpathcdn.com",
+			"openseauserdata.com",
+			"www.penguinfightclub.com",
+			"media.raritysniper.com",
+			"media.npr.org",
+			"images.livemint.com",
+			"dgbijzg00pxv8.cloudfront.net",
+			"nftplazas.com",
+			"pbs.twimg.com",
+			"preview.redd.it",
+		],
+	},
 };
 
 module.exports = nextConfig;
